7dbab640d891d33bf3ecb9b6a47797aeff332d31
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-07-23 22:11:42 +0430

    ✨ feat : Added _newWorkout for making running and cycling feature.

--- src/script/map.ts
+++ src/script/map.ts
@@ -1,14 +1,27 @@
 import L, { Map } from "leaflet";
-
+import { Workout } from "./workout";
+import { Running } from "./running";
+import { Cycling } from "./Cycling";
 const form = <HTMLFormElement>document.querySelector("#form");
-const distance = <HTMLElement>document.querySelector(".mapty__distance");
+const typeWorkout = <HTMLSelectElement>(
+  document.querySelector(".mapty__type-workout")
+);
+const distance = <HTMLInputElement>document.querySelector(".mapty__distance");
+const duration = <HTMLInputElement>document.querySelector(".mapty__duration");
+const cadence = <HTMLInputElement>document.querySelector(".mapty__cadence");
+const elevGain = <HTMLInputElement>document.querySelector(".mapty__elevGain");
+const checkElementType = <HTMLInputElement>(
+  document.querySelector(".type--cadence")
+);
 export default class LeafletMap {
   private _map: Map;
+  private _workouts: Workout[];
   constructor(private Latitude?: number, private Longitude?: number) {
     this.Latitude = Latitude;
     this.Longitude = Longitude;
     this._map = L.map("map");
     this.loadMap();
+    this._workouts = [];
     form.addEventListener("submit", this.formSubmit.bind(this));
   }
 
@@ -32,6 +45,44 @@ export default class LeafletMap {
 
   formSubmit(e: SubmitEvent) {
     e.preventDefault();
+    this._newWorkout();
+    console.log(this._workouts);
+  }
+
+  private _newWorkout() {
+    if (typeWorkout.value === "running") {
+      if (!cadence.value) {
+        const elem: HTMLElement | any = cadence.closest(".mapty__row");
+        const labelEl: HTMLLabelElement | any = elem?.querySelector("label");
+        labelEl.textContent = "Cadence";
+        cadence.classList.remove("mapty__elev");
+        cadence.classList.add("mapty__cadence");
+        cadence.placeholder = "step/min";
+      }
+      const running = new Running(
+        typeWorkout.value,
+        Number.parseInt(distance.value),
+        Number.parseInt(duration.value),
+        Number.parseInt(cadence.value)
+      );
+      this._workouts.push(running);
+    } else if (typeWorkout.value === "cycling") {
+      if (cadence.value) {
+        const elem: HTMLElement | any = cadence.closest(".mapty__row");
+        const labelEl: HTMLLabelElement | any = elem?.querySelector("label");
+        labelEl.textContent = "Elev Gain";
+        cadence.classList.remove("mapty__cadence");
+        cadence.classList.add("mapty__elev");
+        cadence.placeholder = "meters";
+      }
+      const cycling = new Cycling(
+        typeWorkout.value,
+        Number.parseInt(distance.value),
+        Number.parseInt(duration.value),
+        Number.parseInt(elevGain.value)
+      );
+      this._workouts.push(cycling);
+    }
   }
   private _showForm() {
     form.classList.remove("hidden");
