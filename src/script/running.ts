b1f9a6cc4364a45b9b770e9e87fa6877864bb431
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-07-25 15:25:41 +0430

    ✨feat: Added calcPace method in running workout.

--- src/script/running.ts
+++ src/script/running.ts
@@ -1,12 +1,19 @@
 import { Workout } from "./workout";
 
 export class Running extends Workout {
+  private pace?: number = 0;
   constructor(
     public typeWorkout: string,
     public distance: number,
     public duration: number,
-    public cadence: number
+    public cadence?: number
   ) {
     super(typeWorkout, distance, duration);
+    this.calcPace?.();
+  }
+
+  calcPace?(): number {
+    this.pace = this.duration / this.distance;
+    return this.pace;
   }
 }
