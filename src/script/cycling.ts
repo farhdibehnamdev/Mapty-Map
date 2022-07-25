e74a4dddf2963508352d4e51a40f87927830313b
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-07-25 15:25:10 +0430

    ✨feat: Added calcSpeed method in cycling workout.

--- src/script/cycling.ts
+++ src/script/cycling.ts
@@ -1,12 +1,20 @@
 import { Workout } from "./workout";
 
 export class Cycling extends Workout {
+  private speed?: number = 0;
   constructor(
     public typeWorkout: string,
     public distance: number,
     public duration: number,
-    public elevGain: number
+    public elevGain?: number
   ) {
     super(typeWorkout, distance, duration);
+    this.calcSpeed?.();
+  }
+
+  calcSpeed?(): number {
+    this.speed = this.distance / (this.duration / 60);
+    console.log("speed ::", this.speed);
+    return this.speed;
   }
 }
